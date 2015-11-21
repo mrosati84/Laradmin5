be0c1ac167e73357e6dc22efc1f2ba7baa22b525
Matteo Rosati <mrosati@h-art.com>
2015-11-21 17:51:45 +0100

    add base boilerplate libraries for templates

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Laradmin</title>
+    <link rel="stylesheet" href="{{ asset('vendor/laradmin/css/lib/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/laradmin/css/lib/bootstrap-theme.min.css') }}">
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        <div class="row">
+            @yield('content')
+        </div>
+    </div>
+
+    <script src="{{ asset('vendor/laradmin/js/lib/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/laradmin/js/lib/bootstrap.min.js') }}"></script>
 </body>
 </html>
