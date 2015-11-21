fa7a27f51b7f1cda17d8cd5a845caf2076404a9b
Matteo Rosati <mrosati@h-art.com>
2015-11-21 16:05:22 +0100

    add base configuration file

---
+++ config/laradmin.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    // Your app namespace.
+    'models_namespace' => 'Laravel',
+
+    // User-defined admin classes namespace.
+    'admin_classes_namespace' => 'Laravel\Admin',
+
+    // List all the Eloquent models to handle.
+    'models' => [
+        'User' => [],
+        'Book' => [],
+    ]
+
+];
